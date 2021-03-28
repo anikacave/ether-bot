b8da866048c93c1535b37807ca8a92dca7fc1a2d
Alma Thaler <at564@cornell.edu>
2021-03-27 21:08:55 -0400

    mli with signatures, to add skeleton ml soon

--- ether_csv.mli
+++ ether_csv.mli
@@ -1,1 +1,13 @@
-(* create csv method signatures *)
+module type ether_csv = sig
+  (** [create_csv ()] *)
+  val create_csv : unit -> unit
+
+  (** [from_csv time] is the [formatted_str_price_time] of the data
+      written to the csv at [time]. If no such entry exists, raises
+      (TBD) *)
+  val from_csv : float -> string
+
+  (** [update_csv ()] appends the current data to the log file. Creates
+      an intermittent (?) file for safe writing*)
+  val safe_update_csv : unit -> unit
+end
---
+++ ether_scan_processing.mli
@@ -0,0 +1,18 @@
+module type ether_scan_processing = sig
+  (** [get_price_time ()] is the pair (a, b) where a is the float
+      describing the current Ethereum price in USD, and b is the string
+      describing the date at which the Ether price was queried, in
+      [hh:mm:ss day month year]. Errors: TBD *)
+  val get_price_time : unit -> float * string
+
+  (** *)
+  val convert_cur_price : string -> float
+
+  (** *)
+  val convert_time_stamp : string -> string
+
+  (** [formatted_str_price_time ()] returns a cleanly formatted string
+      of the form "Current Price: <price>\nAt Time: <time stamp>" (TBD),
+      for printing to GUI*)
+  val formatted_str_price_time : unit -> string
+end
