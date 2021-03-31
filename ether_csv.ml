8eceecfdd93f85b0b2442fe2ea187f441e9d5e33
Anika Cave <asc287@cornell.edu>
2021-03-31 19:43:59 -0400

    outline csv

--- ether_csv.ml
+++ ether_csv.ml
@@ -1,9 +1,19 @@
+open Ether_scan_processing
+open Csv
+
+(* Filename type *)
+type filename = string
+
+(* Checks if a row being appended/added to a CSV file is a valid row
+   else will throw a TBD error *)
+let is_valid_csv_row (row : string) = true
+
 (** [create_csv ()] *)
-let create_csv un = ()
+let create_csv (file : filename) = ()
 
 (** [from_csv time] is the [formatted_str_price_time] of the data
     written to the csv at [time]. If no such entry exists, raises (TBD) *)
-let from_csv flt = "TODO"
+let from_csv (flt : float) (file : filename) = "TODO"
 
 (** [update_csv ()] appends the current data to the log file. Creates an
     intermittent (?) file for safe writing*)
--- ether_csv.mli
+++ ether_csv.mli
@@ -1,10 +1,15 @@
-(** [create_csv ()] *)
-val create_csv : unit -> unit
+(** The type filename *)
+type filename = string
+
+(** [create_csv ()] creates csv @ given filename (will be from current
+    data availible and should be only one line long to include the most
+    current information) *)
+val create_csv : filename -> unit
 
 (** [from_csv time] is the [formatted_str_price_time] of the data
     written to the csv at [time]. If no such entry exists, raises (TBD) *)
-val from_csv : float -> string
+val from_csv : float -> filename -> string
 
 (** [update_csv ()] appends the current data to the log file. Creates an
     intermittent (?) file for safe writing*)
-val safe_update_csv : unit -> unit
+val safe_update_csv : filename -> unit
