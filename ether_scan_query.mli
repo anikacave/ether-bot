c291ee41f367c500eacd49eaeccf3ca5c7411912
Eric <eds228@cornell.edu>
2021-04-01 19:22:55 -0400

    small change to the ether_scan_query mli file

--- ether_scan_query.mli
+++ ether_scan_query.mli
@@ -1,6 +1,7 @@
-(** [get_cure_price ()] is the pair (a, b) where a is the string
+(** [get_cur_price ()] is the pair (a, b) where a is the string
     describing the current Ethereum price in USD, and b is the string of
-    an integer representing the current Epoch time. Errors: TBD *)
+    an integer representing the Epoch timestamp of the last price. 
+    @throws Query_Failed *)
 val get_cur_price : unit -> string * string
 
 (* if a query to etherscan.io fails, Query_Failed will be raised 
