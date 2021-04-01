de1e362958d1e1c2e6fe03091e670980e6d5c056
Anika Cave <asc287@cornell.edu>
2021-04-01 12:01:02 -0400

    many updates to csv functionality working on copy paste currently not handling end of line

--- ether_csv.ml
+++ ether_csv.ml
@@ -5,7 +5,7 @@ open Csv
 type filename = string
 
 (* TODO price will be from the get_price_time will ahve to format that *)
-let row_header = "number, formatted date, formatted time, price"
+let row_header = "formatted time, price\n"
 
 (* Checks if a row being appended/added to a CSV file is a valid row
    else will throw a TBD error -- also understood that this is terrible
@@ -13,17 +13,59 @@ let row_header = "number, formatted date, formatted time, price"
    check of whether or not a csv row is valid*)
 let is_valid_csv_row (row : string) =
   let elems = String.split_on_char ',' row in
-  if List.length elems = 4 then false else false
+  if List.length elems = 2 then false else false
+
+(** Makes a formatted row of the csv file by calling functions from
+    ether_scan_processing and converting into comma seperated values *)
+let make_csv_row un =
+  let price, time = get_price_time () in
+  string_of_float price ^ "," ^ time ^ "\n"
+
+(* Prints to CSV output channel if header is marked as true, then a
+   header will be appended to the beginning of the channel otherwise it
+   will be assumed that the file is already properly labelled *)
+let csv_line channel header =
+  let bytes2 =
+    if header then row_header ^ make_csv_row () else make_csv_row ()
+  in
+  output_string channel bytes2
 
 (** [create_csv ()] creates csv @ given filename (will be from current
     data availible and should be only one line long to include the most
     current information) *)
-let create_csv (file : filename) = ()
+let create_csv (file : filename) =
+  let buff = open_out file in
+  csv_line buff true;
+  Stdlib.close_out buff
 
-(** [from_csv time] is the [formatted_str_price_time] of the data
-    written to the csv at [time]. If no such entry exists, raises (TBD) *)
+(* Feeds information from one file to an output stream to copy the
+   contents of a file into another *)
+let rec copy_paste input_stream output_stream =
+  let read = input_line input_stream in
+  match read with
+  | "" -> flush output_stream
+  | h ->
+      output_string output_stream (h ^ "\n");
+      flush output_stream;
+      copy_paste input_stream output_stream
+
+(* updates the filename for any type of file (not just csv) so we can
+   write to text files, etc, assumes that a file DOES have an extension
+   TODO throw invalid file extension*)
+let update_file_name (file : filename) =
+  let elems = String.split_on_char '.' file in
+  if List.length elems = 2 then
+    List.nth elems 0 ^ "*." ^ List.nth elems 1
+  else "TODO raise error"
+
+(* [from_csv time] is the price of the ethereum at given [time]. If no
+   such entry exists, raises (TBD) *)
 let from_csv (flt : float) (file : filename) = "TODO"
 
-(** [update_csv ()] appends the current data to the log file. Creates an
-    intermittent (?) file for safe writing*)
-let safe_update_csv un = ()
+(* [update_csv ()] appends the current data to the a specified csv file.
+   Writes to a seperate file than the original one specified to avoid
+   deleting all data from a csv file*)
+let safe_update_csv file =
+  let input_stream = open_in file in
+  let output_stream = open_out (update_file_name file) in
+  copy_paste input_stream output_stream
--- ether_csv.mli
+++ ether_csv.mli
@@ -1,15 +1,18 @@
 (** The type filename *)
 type filename = string
 
+(* TODO fix the type here *)
+
 (** [create_csv ()] creates csv @ given filename (will be from current
     data availible and should be only one line long to include the most
     current information) *)
 val create_csv : filename -> unit
 
-(** [from_csv time] is the [formatted_str_price_time] of the data
-    written to the csv at [time]. If no such entry exists, raises (TBD) *)
+(** [from_csv time] is the price of the ethereum at given [time]. If no
+    such entry exists, raises (TBD) *)
 val from_csv : float -> filename -> string
 
-(** [update_csv ()] appends the current data to the log file. Creates an
-    intermittent (?) file for safe writing*)
+(** [update_csv ()] appends the current data to the a specified csv
+    file. Writes to a seperate file than the original one specified to
+    avoid deleting all data from a csv file*)
 val safe_update_csv : filename -> unit
