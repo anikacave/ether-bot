0fc0c998ea00a25f1a1a2abb00ea24dcc10621d0
Anika Cave <asc287@cornell.edu>
2021-04-01 16:10:57 -0400

    doc

--- ether_csv.mli
+++ ether_csv.mli
@@ -13,12 +13,13 @@ exception InvalidFileExtensionFormat
     current information) *)
 val create_csv : filename -> unit
 
-(** [from_csv time] is the price of the ethereum at given [time]. If no
-    such entry exists, raises (TBD) *)
+(** [from_csv time file] is the price of the ethereum at given [time].
+    If no such entry exists, raises TimeStampNotFound *)
 val from_csv : float -> filename -> string
 
 (** [update_csv file] appends the current data to the a specified csv
     file. Writes to a seperate file than the original one specified to
     avoid deleting all data from a csv file. returns the name of the new
-    file that was written to*)
+    file that was written to. Raises InvalidFileExtensionFormat if the
+    file extension is illegal*)
 val safe_update_csv : filename -> filename
