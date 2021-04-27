607867913d982582ed3a48acc96e940a316cd201
Kyra Gunluk <kg556@cornell.edu>
2021-04-26 20:44:29 -0400

    unimplemented indicators

---
+++ indicators.ml
@@ -0,0 +1,33 @@
+(** this file calculates indicator values from raw price data *)
+
+(* a list of tuples of epoch time and USD price from csv_data_bot
+   Requires: the time is ordered from newest to oldest *)
+
+(* represents the raw data to calculate indicators from *)
+type dataset = int * float list
+
+(* parses a csv file and constructs dataset formatter describes how to
+   parse each line into a tuple [from_csv formatter file_name] is a
+   dataset from the file*)
+let from_csv = failwith "unimplemented"
+
+(* constructs a dataset from a list of tuples *)
+let from_tuple_list lst = failwith "unimlemented"
+
+(* returns a subset of the dataset from [trim dataset begin end] is a
+   dataset including datapoints between begin and end inclusive *)
+let trim t start finish = failwith "unimplemented"
+
+(* [sma dataset period] is the SMA of the dataset given the desired
+   period*)
+let sma t period = failwith "unimplemented"
+
+(* [stoch data] is the stochastic oscillator (indicator) with lookback
+   period of 14 days and with closing time of ~11:59*)
+let stoch t = failwith "unimplemented"
+
+(* calculates adx *)
+let adx t = failwith "unimplemented"
+
+(* calculates macd *)
+let macd t = failwith "unimplemented"
